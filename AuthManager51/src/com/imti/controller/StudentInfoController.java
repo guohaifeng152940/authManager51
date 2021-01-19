91bd6e7302ae1a510682ab737d9b65a8714a15c5
admin <1191619897@qq.com>
2021-01-19 13:52:04 +0800

    添加学生controller层

---
+++ AuthManager51/src/com/imti/controller/StudentInfoController.java
@@ -0,0 +1,16 @@
+package com.imti.controller;
+/**@文件名: StudentInfoController.java
+ * @类功能说明: 
+ * @作者: GuoHaiFeng
+ * @Email: 1191619897@qq.com
+ * @日期: 2021年1月19日下午1:51:29
+ * @修改说明:<br> 
+ * <pre>
+ * 	 <li>作者: GuoHaiFeng</li> 
+ * 	 <li>日期: 2021年1月19日下午1:51:29</li> 
+ *	 <li>内容: </li>
+ * </pre>
+ */
+public class StudentInfoController {
+
+}
